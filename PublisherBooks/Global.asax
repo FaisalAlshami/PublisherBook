f8f2140bfa706f66df19ad4b3a6606638c77440e
FaisalAlshami <fasali2010@gmail.com>
2021-08-08 14:59:10 +0800

    Add files via upload

---
+++ PublisherBooks/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PublisherBooks.MvcApplication" Language="C#" %>
---
+++ PublisherBooks/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Owin;
+using Owin;
+using PublisherBooks.Models;
+
+[assembly: OwinStartupAttribute(typeof(PublisherBooks.Startup))]
+namespace PublisherBooks
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+           DataAccess DbContext = new DataAccess();
+            if (DbContext.ConnectState.ToLower().Contains("error"))
+            {
+               
+            }
+            //   ConfigureAuth(app);
+        }
+    }
+}
